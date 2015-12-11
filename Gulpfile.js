973975c4d8349099bc61adc2097f3fd3778d5803
Coding Jerk <codingjerk@gmail.com>
2015-12-11 04:48:47 +0300

    Travis fixes

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var mocha = require('gulp-mocha');
+var coveralls = require('gulp-coveralls');
 
 gulp.task('build', function() {
     return gulp.src('src/*.js')
@@ -16,3 +17,10 @@ gulp.task('test', function() {
 gulp.task('watch', function() {
     gulp.watch('src/*.js', ['build', 'test']);
 });
+
+gulp.task('coveralls', function() {
+    return gulp.src('./coverage/lcov.info')
+        .pipe(coveralls());
+});
+
+gulp.task('default', ['build', 'test', 'coveralls']);
