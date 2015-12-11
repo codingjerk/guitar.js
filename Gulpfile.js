03d70de9bff86565b3dde22cc4668b5b3798758b
Coding Jerk <codingjerk@gmail.com>
2015-12-11 05:20:51 +0300

    Renaming in gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,10 +2,14 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var mocha = require('gulp-mocha');
 var coveralls = require('gulp-coveralls');
+var rename = require('gulp-rename');
 
 gulp.task('build', function() {
     return gulp.src('src/*.js')
         .pipe(uglify())
+        .pipe(rename(function(path) {
+            path.extname = ".min.js";
+        }))
         .pipe(gulp.dest('build'));
 });
 
