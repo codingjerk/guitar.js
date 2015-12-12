9e7ea2f05c7006f871e88f007f5ab76a2aae398e
failuref <chezstov@gmail.com>
2015-12-12 05:18:56 +0300

    Basic docs added

---
+++ docs/conf.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shlex
+
+extensions = []
+templates_path = ['_templates']
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+project = 'guitar.js'
+copyright = '2015, Denis Gruzdev <chezstov@gmail.com>'
+author = 'Denis Gruzdev <chezstov@gmail.com>'
+
+version = '0.1.2'
+release = '0.1.2'
+
+language = 'JavaScript'
+
+exclude_patterns = ['_build']
+
+pygments_style = 'sphinx'
+
+todo_include_todos = False
+
+html_theme = 'default'
+
+html_static_path = ['_static']
+
+htmlhelp_basename = 'guitarjsdoc'
+
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_elements = {
+# The paper size ('letterpaper' or 'a4paper').
+#'papersize': 'letterpaper',
+
+# The font size ('10pt', '11pt' or '12pt').
+#'pointsize': '10pt',
+
+# Additional stuff for the LaTeX preamble.
+#'preamble': '',
+
+# Latex figure (float) alignment
+#'figure_align': 'htbp',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+  (master_doc, 'guitarjs.tex', 'guitar.js Documentation',
+   'Denis Gruzdev \\textless{}chezstov@gmail.com\\textgreater{}', 'manual'),
+]
+
+# The name of an image file (relative to this directory) to place at the top of
+# the title page.
+#latex_logo = None
+
+# For "manual" documents, if this is true, then toplevel headings are parts,
+# not chapters.
+#latex_use_parts = False
+
+# If true, show page references after internal links.
+#latex_show_pagerefs = False
+
+# If true, show URL addresses after external links.
+#latex_show_urls = False
+
+# Documents to append as an appendix to all manuals.
+#latex_appendices = []
+
+# If false, no module index is generated.
+#latex_domain_indices = True
+
+
+# -- Options for manual page output ---------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    (master_doc, 'guitarjs', 'guitar.js Documentation',
+     [author], 1)
+]
+
+# If true, show URL addresses after external links.
+#man_show_urls = False
+
+
+# -- Options for Texinfo output -------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+  (master_doc, 'guitarjs', 'guitar.js Documentation',
+   author, 'guitarjs', 'One line description of project.',
+   'Miscellaneous'),
+]
+
+# Documents to append as an appendix to all manuals.
+#texinfo_appendices = []
+
+# If false, no module index is generated.
+#texinfo_domain_indices = True
+
+# How to display URL addresses: 'footnote', 'no', or 'inline'.
+#texinfo_show_urls = 'footnote'
+
+# If true, do not generate a @detailmenu in the "Top" node's menu.
+#texinfo_no_detailmenu = False
